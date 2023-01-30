e0e8897e12c1af3c36c5f068dc2478bfd8a4c601
Manikantanagasai H. Illuri <milluri@ucsc.edu>
2023-01-30 07:31:13 +0000

    DONEDONE

--- asgn2/euler.c
+++ asgn2/euler.c
@@ -5,18 +5,13 @@
 
 static double count = 2.0;
 
-double po(double x) {
-    double ans = x * x;
-    return ans;
-}
-
 double pi_euler(void) {
     double fnl = 1.0;
     double f = 1.0;
     double cs;
 
     for (; f > EPSILON; count += 1.0) {
-        cs = po(count);
+        cs = count * count;
         f = 1.0 / cs;
         fnl += f;
     }
