8f981fae8820f2e3d39acce6941b085b33338eba
Manikantanagasai H. Illuri <milluri@ucsc.edu>
2023-01-30 06:04:14 +0000

    DONE DONE

--- asgn2/bbp.c
+++ asgn2/bbp.c
@@ -5,16 +5,6 @@
 
 static double count = 0.0;
 
-double pow(double b, double expo) {
-    double fnl = b;
-    for (double i = 0.0; i <= expo; i += 1.0) {
-        if (b != 0 && expo == 0) {
-            return 1.0;
-        }
-        fnl = fnl * b;
-    }
-    return (1.0 / fnl);
-}
 
 double pi_bbp(void) {
     double fnl = 1.0;
@@ -23,15 +13,18 @@ double pi_bbp(void) {
     double two;
     double three;
     double four;
-    double tmp;
+
 
     for (; absolute(fnl) > EPSILON; count += 1.0) {
-        tmp = pow(16, count);
+
         one = 4.0 / ((8.0 * count) + 1.0);
         two = 2.0 / ((8.0 * count) + 4.0);
         three = 1.0 / ((8.0 * count) + 5.0);
         four = 1.0 / ((8.0 * count) + 6.0);
-        fnl = tmp * (one - two - three - four);
+        fnl = (one - two - three - four);
+        for(int i = 0; i < count; i++){
+        	fnl = fnl / 16;	
+        }
         tot += fnl;
     }
     return tot;
